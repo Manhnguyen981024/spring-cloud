7679924addee07e938f7ced8a84d1ea7ad2bac2e
0100632620 <nguyencaomanh@cathaylife.com.vn>
2024-09-17 16:35:40 +0700

    add circuit breaker

--- currency-exchange-service/src/main/java/com/manhpro/springcloud/currency_exchange_service/currency_exchange_service/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/manhpro/springcloud/currency_exchange_service/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -1,18 +1,26 @@
 package com.manhpro.springcloud.currency_exchange_service.currency_exchange_service.controller;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.manhpro.springcloud.currency_exchange_service.currency_exchange_service.bean.CurrencyExchangeBean;
 import com.manhpro.springcloud.currency_exchange_service.currency_exchange_service.repository.CurrencyExchangeRepository;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @RestController
 public class CurrencyExchangeController {
+	private static final Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
+
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
 
@@ -32,5 +40,20 @@ public class CurrencyExchangeController {
 		currencyExchange.setEnvironment(port);
 		
 		return currencyExchange;
+	}
+	
+	@GetMapping("/circuit-breaker")
+	/* @Retry(name = "circuit-breaker", fallbackMethod = "callback") */
+//	@CircuitBreaker(name = "default", fallbackMethod = "callback")
+//	@RateLimiter(name = "same-api")
+	@Bulkhead(name = "same-api", fallbackMethod = "callback")
+	public String applyCircuitBreaker() {
+		logger.info("receive request -> ");
+		String response = new RestTemplate().getForEntity("http://localhost:8080/test", String.class).getBody();
+		return response;
+	}
+	
+	public String callback(Exception e) {
+		return "apply circuit-breaker";
 	}
 }
